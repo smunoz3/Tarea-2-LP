6f9399d7f8454139f8c7298c685bc1cfd06d6c10
smunoz3 <sebamunoz003@gmail.com>
2023-09-02 12:03:18 -0400

    ??

--- Codigo/Tablero.c
+++ Codigo/Tablero.c
@@ -6,7 +6,32 @@ void*** tablero;
 int dimension;
 
 void IniciarTablero(int n){
-    // Su codigo
+    //struct Tierra *tierra1;
+    //tierra1->vida = 1;
+    dimension = n;
+    struct Tierra ***tablero = malloc(n* sizeof(struct Tierra**));
+    if (tablero == NULL){
+        perror("error memoria");
+        exit(1);
+    }
+    for(int i = 0;i<n; i++){
+        tablero[i]=malloc(n* sizeof(struct Tierra*));
+        if (tablero[i] == NULL){
+            perror("error memoria");
+            exit(1);
+        }
+    }
+    for(int i = 0;i<n; i++){
+        for(int j = 0;j<n;j++){
+            tablero[i][j]=malloc(n*sizeof(Tierra));
+            if (tablero[i][j] == NULL){
+                perror("error memoria");
+                exit(1);
+            }
+            tablero[i][j]->vida = 1;
+            tablero[i][j]->es_tesoro = 0;
+        }
+    }
     return;
 }
 
@@ -41,6 +66,15 @@ void VerTesoros(){
 
 
 void BorrarTablero(){
-    // Su codigo
+    for(int i = 0;i<dimension; i++){
+        for(int j = 0;j<dimension;j++){
+            if (tablero[i][j] == NULL){
+                printf("ERROR");
+            }
+            free(tablero[i][j]);
+        }
+        free(tablero[i]);
+    }
+    free(tablero);
     return;
 }
--- Codigo/Tablero.h
+++ Codigo/Tablero.h
@@ -6,7 +6,7 @@ extern int dimension; // 7 en 7x7, 10 en 10x10, 12 en 12x12
 
 void IniciarTablero(int n);
 void PasarTurno();
-void ColocarBomba(Bomba* b, int fila, int columna);
+//void ColocarBomba(Bomba* b, int fila, int columna);
 void MostrarTablero();
 void MostrarBombas();
 void VerTesoros();
