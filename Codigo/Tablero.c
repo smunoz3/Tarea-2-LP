f37f74ab6df1f50851c35aea45a45034be437c4a
smunoz3 <sebamunoz003@gmail.com>
2023-09-03 15:18:17 -0400

    matriz

--- Codigo/Tablero.c
+++ Codigo/Tablero.c
@@ -2,36 +2,47 @@
 #include "Tablero.h"
 #include "Bomba.h"
 
+
 void*** tablero;
 int dimension;
 
 void IniciarTablero(int n){
-    //struct Tierra *tierra1;
-    //tierra1->vida = 1;
     dimension = n;
-    struct Tierra ***tablero = malloc(n* sizeof(struct Tierra**));
-    if (tablero == NULL){
-        perror("error memoria");
-        exit(1);
+    srand(time(NULL));
+
+    tablero = (void***)malloc(n * sizeof(void**));
+    for(int i = 0;i<n;i++){
+        tablero[i] = (void**)malloc(n * sizeof(void*));
     }
+
+    
+    
     for(int i = 0;i<n; i++){
-        tablero[i]=malloc(n* sizeof(struct Tierra*));
-        if (tablero[i] == NULL){
-            perror("error memoria");
-            exit(1);
+        for(int j = 0;j<n;j++){
+            tablero[i][j] = (void*)malloc(sizeof(void));
+            
+            struct Tierra terra;
+            //terra.vida = 2;
+            printf("%d",terra.vida);
+            
+            printf("\n");
+            //terra.es_tesoro = 0;
+
+
+            tablero[i][j]= &terra;
+            // printf("%p",&tablero[i][j]);
+            // printf("\n");
         }
+
     }
     for(int i = 0;i<n; i++){
         for(int j = 0;j<n;j++){
-            tablero[i][j]=malloc(n*sizeof(Tierra));
-            if (tablero[i][j] == NULL){
-                perror("error memoria");
-                exit(1);
-            }
-            tablero[i][j]->vida = 1;
-            tablero[i][j]->es_tesoro = 0;
+            tablero[i][j];
         }
     }
+    printf("%p\n",tablero[1][0]);
+    struct Tierra contenido = tablero[1][0];
+    printf("fin iniciar tablero \n");
     return;
 }
 
@@ -48,7 +59,6 @@ void ColocarBomba(Bomba* b, int fila, int columna){
 
 
 void MostrarTablero(){
-    // Su codigo
     return;
 }
 
@@ -68,11 +78,10 @@ void VerTesoros(){
 void BorrarTablero(){
     for(int i = 0;i<dimension; i++){
         for(int j = 0;j<dimension;j++){
-            if (tablero[i][j] == NULL){
-                printf("ERROR");
-            }
             free(tablero[i][j]);
         }
+    }
+    for(int i = 0;i<dimension;i++){
         free(tablero[i]);
     }
     free(tablero);
